f06b20d19a60e02384d89db5f5ec2fa4c1f59ef7
ilson28 <ilsondiazmorelo28@gmail.com>
2025-04-08 15:39:34 -0500

    app config

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request,redirect, render_template
+from config.db import app
+
+#en esta parte trabajaremos la importacion de rutas 
+
+#
+
+@app.router("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug==True, port=5000,host="0.0.0.0")
