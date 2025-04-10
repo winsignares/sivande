d03a8eb81acbef6313ad3efe3be037662ab8e885
ilson28 <ilsondiazmorelo28@gmail.com>
2025-04-10 15:50:05 -0500

    requirements finished

--- app/app.py
+++ app/app.py
@@ -2,8 +2,15 @@ from flask import Flask, request,redirect, render_template
 from config.db import app
 
 #en esta parte trabajaremos la importacion de rutas 
+from api.UserApi import ruta_user
+from api.CategoryApi import ruta_category
+from api.TaskApi import ruta_task
 
-#
+#registramos las rutas con blueprint
+app.register_blueprint(ruta_user, url_prefix="api")
+app.register_blueprint(ruta_task, url_prefix="api")
+app.register_blueprint(ruta_category, url_prefix="api")
+
 
 @app.router("/")
 def index():
